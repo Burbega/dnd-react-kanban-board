8939d4da376d3fb1f1ec0edd4fe69ca7da3ea468
Rodrigo Mesquita <46407362+Burbega@users.noreply.github.com>
2020-11-20 12:26:07 -0300

    Drag and Drop handler added.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { v4 as uuid } from 'uuid';
 import './app.css';
 
@@ -24,6 +26,42 @@ const columnsFromBackend = {
   [uuid()]: {
     name: 'Done',
     items: []
+  }
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if(!result.destination) return;
+  const { source, destination } = result;
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    })
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
   }
 };
 
