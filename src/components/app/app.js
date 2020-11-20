25f3480e6596d159cba28ea67deca20937407d6c
Rodrigo Mesquita <46407362+Burbega@users.noreply.github.com>
2020-11-20 12:20:07 -0300

    Variables for tasks and columns created.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,31 @@
+import { v4 as uuid } from 'uuid';
 import './app.css';
+
+const itemsFromBackend = [
+  { id: uuid(), content: 'First Task' },
+  { id: uuid(), content: 'Second Task' },
+  { id: uuid(), content: 'Third Task' },
+  { id: uuid(), content: 'Fourth Task' }
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: 'Requested',
+    items: itemsFromBackend
+  },
+  [uuid()]: {
+    name: 'To Do',
+    items: []
+  },
+  [uuid()]: {
+    name: 'In Progress',
+    items: []
+  },
+  [uuid()]: {
+    name: 'Done',
+    items: []
+  }
+};
 
 function App() {
   return (
